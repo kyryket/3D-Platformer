547f99631728889b41372bd0418d4e7a7abe4594
kyryket <157015768+kyryket@users.noreply.github.com>
2024-01-23 18:00:55 +0600

    Add files via upload

---
+++ PlayerHp.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHp : MonoBehaviour
+{
+    public int health = 10;
+
+     public void TakeDamage(int damage)
+     {
+       health -= damage;
+       print("Player hp:" + health);
+     }
+}
