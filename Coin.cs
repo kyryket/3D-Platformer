c509fcab7d3b263ff5d46327aaf66daa022cd70a
kyryket <157015768+kyryket@users.noreply.github.com>
2024-01-23 19:10:58 +0600

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+       Player player = other.GetComponent<Player>();
+
+       player.CollectCoins();
+
+       Destroy(gameObject);
+    }
+}
